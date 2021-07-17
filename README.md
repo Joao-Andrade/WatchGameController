598e919de1946f175aae29bd4a538f40be7139a6
Joao Andrade <joaopcandrade@gmail.com>
2021-07-17 16:24:25 +0100

    Update documentation and tizen project.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This project allows for anyone with a smartwatch with Tizes OS (for example, Sam
 
 Simply put, the way it works is that the smartwatch sends the gravity sensor values, every 100ms, to the computer through an API, which in turn updates an xbox virtual controller that should control the game's vehicle.
 
----- diagram showing that ----
+![Diagram](documentation/GameControllerDiagram.png)
 
 When everything is connected and ready to play (check (how to Install)[#how-to-install]), the game should be controlled by doing the following movements (assuming the user has the watch on the wrist).
 
@@ -26,7 +26,7 @@ On my experience, the best starting position (although there may be variations t
 
   ---- image showing that ----
 
- And that's it.
+ And that's it. This simulates the controller thumbstick (direction) and the back triggers (speed and break) so the car reflects the user actions.
 
 ## How to install
 
@@ -44,4 +44,6 @@ In order to place the app on the smartwatch, it is necessary to have on the PC s
 
 ### Smartwatch app
 
-To place the app on the smartphone, after having conenction between the smartwatch and Tizen Studio ([check official documentation](https://docs.tizen.org/application/web/get-started/wearable/first-app/#running-on-a-target-device)...
+After having conenction between the smartwatch and Tizen Studio ([check pre requisites](#pre-requisites), in the Tizen Studio menu, select "File > Import". Select "Tizen > Tizen Project", click "Next" and select the folder from this github project "tizen_app". Click "Next" and select the "WatchGameController" project and click "Finish" to import it. The project should appear on the project explorer of Tizen Studio and the code can be analysed/changed from there.
+
+[!import_project](documentation/import_project.png)
--- tizen_app/js/app.js
+++ tizen_app/js/app.js
@@ -1,98 +1,51 @@
 ( function () {
 
+	// initialize some variables.
+	var button_home_play = document.getElementById('button-home-play');
+	var input_server_address = document.getElementById('input-server-address');
+	var input_server_endpoint = document.getElementById('input-server-endpoint');
+	// request to server initializations.
 	var setup_mode_xhttp;
 	setup_mode_xhttp=new XMLHttpRequest();
 	setup_mode_xhttp.responseType = 'json';
+	// if it should send data to server or not.
+	var send_data = false;
+	
+	// initialize sensor function.
 	function onSensorStart()
 	{
 	  console.log("Gravity sensor started.");
 	}
+	// what happens when the collects sensor data.
 	function onGetSensorSuccess(sensorData)
 	{
-		setup_mode_data = JSON.stringify({"reset": false, "x": parseInt(sensorData.x * 100), "y": parseInt(sensorData.y * 100), "z": parseInt(sensorData.z * 100)});
+		if (!send_data) {
+			return false;
+		}
+		setup_mode_xhttp.open("POST", input_server_address.value);
+		setup_mode_xhttp.setRequestHeader("Content-Type", "application/json");
+		setup_mode_data = JSON.stringify({"x": parseFloat(sensorData.x), "y": parseFloat(sensorData.y), "z": parseFloat(sensorData.z)});
 		setup_mode_xhttp.send(setup_mode_data);
 	}
+	
+	// sensor had an error.
 	function onGetSensorError(error)
 	{
 		console.log("Error occurred: " + error.message);
 	}
-	var sensors = tizen.sensorservice.getAvailableSensors();
-	console.log('Available sensor: ' + sensors.toString());
 
-	var gravitySensor = tizen.sensorservice.getDefaultSensor("ACCELERATION");
+	// initialize sensor.
+	var gravitySensor = tizen.sensorservice.getDefaultSensor("GRAVITY");
 	gravitySensor.start(onSensorStart, onGetSensorError);
-	var setup_complete = false;
-	var setup_ongoing = false;
-	// buttons
-	var button_home_setup = document.getElementById('button-home-setup');
-	var button_home_play = document.getElementById('button-home-play');
-	var button_setup_mode = document.getElementById('button-setup-mode');
-	var button_play_mode = document.getElementById('button-play-mode');
-	// texts
-	var text_setup_mode = document.getElementById('text-setup-mode');
-	var text_play_mode = document.getElementById('text-play-mode');
+	gravitySensor.setChangeListener(onGetSensorSuccess, 100);
+	
 	// buttons actions
-	button_home_setup.addEventListener('click', function() {
-		if (setup_complete) {
-			text_setup_mode.innerHTML = "Setup already completed";
-			button_setup_mode.innerHTML = "Setup again";
-		};
-		tau.changePage("#setup_page");
-	});
-	button_setup_mode.addEventListener('click', function() {
-		if (setup_complete && button_setup_mode.innerHTML == "Setup again") {
-			setup_ongoing = false;
-			setup_complete = false;
-			text_setup_mode.innerHTML = "Please setup device";
-			button_setup_mode.innerHTML = "start setup";
-		}
-		if (!setup_complete && !setup_ongoing){
-			setup_mode_xhttp.open("POST", "http://192.168.1.20:5000/api/v1/setup_sensors");
-			setup_mode_xhttp.setRequestHeader("Content-Type", "application/json");
-			setup_mode_data = JSON.stringify({"reset": true});
-			setup_mode_xhttp.onload  = function() {
-				if (setup_mode_xhttp.status != 200) {
-					text_setup_mode.innerHTML = "Something went wrong. Try again.";
-					button_setup_mode.innerHTML = "start setup";
-					return false;
-				}
-				var jsonResponse = setup_mode_xhttp.response;
-				console.log(JSON.stringify(jsonResponse));
-				text_setup_mode.innerHTML = jsonResponse.Message + "<br/>Repetitions left: " + jsonResponse.repetitions_left;
-				button_setup_mode.innerHTML = "Send sensor data";
-				setup_ongoing = true;
-			};
-			setup_mode_xhttp.send(setup_mode_data);
-		} else if (setup_ongoing) {
-			setup_mode_xhttp.open("POST", "http://192.168.1.20:5000/api/v1/setup_sensors");
-			setup_mode_xhttp.setRequestHeader("Content-Type", "application/json");
-			setup_mode_xhttp.onload  = function() {
-				if (setup_mode_xhttp.status != 200) {
-					text_setup_mode.innerHTML = "Something went wrong. Try again.";
-					return false;
-				}
-				var jsonResponse = setup_mode_xhttp.response;
-				if (jsonResponse.Completed) {
-					setup_ongoing = true;
-					setup_complete = true;
-					text_setup_mode.innerHTML = "Setup completed. Feel free to return to the previous menu.";
-					button_setup_mode.innerHTML = "Setup again";
-					return true;
-				};
-				console.log(JSON.stringify(jsonResponse));
-				text_setup_mode.innerHTML = jsonResponse.Message + "<br/>Repetitions left: " + jsonResponse.repetitions_left;
-			};
-			gravitySensor.getGravitySensorData(onGetSensorSuccess, onGetSensorError);
-		};
-	});
 	button_home_play.addEventListener('click', function() {
-		tau.changePage("#play_page");
+		send_data = !send_data;
+		button_home_play.innerHTML = send_data ? "Stop" : "Play"
 	});
-	button_play_mode.addEventListener('click', function() {
-		console.log("A")
-	});
-
-	// exits stuffs 
+	
+	// exits stuffs
 	window.addEventListener( 'tizenhwkey', function( ev ) {
 		if( ev.keyName === "back" ) {
 			var page = document.getElementsByClassName( 'ui-page-active' )[0],
